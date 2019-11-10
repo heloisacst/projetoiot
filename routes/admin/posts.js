c43e5f2fa0c1989f5b603d6183b14961a781f4d0
heloisacst <heloisacst@hotmail.com>
2019-11-10 15:20:27 -0300

    Alterações Projeto IOT

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var postsService = require('../../services/postsService');
-var upload = require('../../middlewares/uploaderMiddleware');
 
 router.get('/', function (req, res, next){
     var posts = postsService.getPosts();
@@ -17,7 +16,7 @@ router.get('/create', function (req, res, next){
     res.render('admin/posts/create');
 });
 
-router.post('/create', upload.single('images'), function (req, res, next){
+router.post('/create', function (req, res, next){
     var posts = postsService.getPosts();
 
     var newId = posts.lenght + 1;
@@ -25,7 +24,6 @@ router.post('/create', upload.single('images'), function (req, res, next){
     var newPost = {};
     newPost.id = newId;
     newPost.title = req.body.title;
-    newPost.image = req.file.filename;
     newPost.description = req.body.description;
     newPost.body = req.body.postBody;
 
